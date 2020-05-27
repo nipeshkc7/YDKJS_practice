65fe3466c4371a71c71ec383e8b1a7f37a9221af
nipeshkc7 <nipeshkc7@gmail.com>
2020-05-28 09:15:39 +1000

    GCClosures.js

---
+++ Scopes and Closures/GCClosures.js
@@ -0,0 +1,48 @@
+// Important to unsubscribe from closures, so that all the discarded references are Garbage collected !!
+
+function manageBtnClickEvents(btn) {
+    var clickHandlers = [];
+
+    return function listener(cb){
+        if (cb) {
+            let clickHandler =
+                function onClick(evt){
+                    console.log("clicked!");
+                    cb(evt);
+                };
+            clickHandlers.push(clickHandler);
+            btn.addEventListener(
+                "click",
+                clickHandler
+            );
+        }
+        else {
+            // passing no callback unsubscribes
+            // all click handlers
+            for (let handler of clickHandlers) {
+                btn.removeEventListener(
+                    "click",
+                    handler
+                );
+            }
+
+            clickHandlers = [];
+        }
+    };
+}
+
+// var mySubmitBtn = ..
+var onSubmit = manageBtnClickEvents(mySubmitBtn);
+
+onSubmit(function checkout(evt){
+    // handle checkout
+});
+
+onSubmit(function trackAction(evt){
+    // log action to analytics
+});
+
+// later, unsubscribe all handlers:
+onSubmit();
+
+// and the checkout() and trackAction() references are Garbage collected !!
