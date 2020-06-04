2710538a337ce0f494a1cd1638078bb7a1879773
nipeshkc7 <nipeshkc7@gmail.com>
2020-06-04 10:26:10 +1000

    closurePart1.js

---
+++ Scopes and Closures/practiceQ/closurePart1.js
@@ -0,0 +1,50 @@
+// Cache to avoid repeated calls to isPrime
+// Also cache factorize() to avoid repeated calls to itself
+
+var isPrime = (function isPrime(v) {
+  var primes = {};
+
+  return function isPrime(v) {
+    if (v in primes) {
+      return primes[v];
+    }
+    if (v <= 3) {
+      return (primes[v] = v > 1);
+    }
+    if (v % 2 == 0 || v % 3 == 0) {
+      return (primes[v] = false);
+    }
+    let vSqrt = Math.sqrt(v);
+    for (let i = 5; i <= vSqrt; i += 6) {
+      if (v % i == 0 || v % (i + 2) == 0) {
+        return (primes[v] = false);
+      }
+    }
+    return (primes[v] = true);
+  };
+})();
+
+var factorize = (function factorize(v) {
+  var factors = {};
+
+  return function findFactors(v) {
+    if (v in factors) {
+      return factors[v];
+    }
+    if (!isPrime(v)) {
+      let i = Math.floor(Math.sqrt(v));
+      while (v % i != 0) {
+        i--;
+      }
+      return (factors[v] = [...findFactors(i), ...findFactors(v / i)]);
+    }
+    return (factors[v] = [v]);
+  };
+})();
+
+console.log(isPrime(11)); // true
+console.log(isPrime(12)); // false
+
+console.log(factorize(11)); // [ 11 ]
+console.log(factorize(12)); // [ 3, 2, 2 ] --> 3*2*2=12
+console.log(factorize(25));
